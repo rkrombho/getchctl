0587fea95347018bbe3263dabb7c02e0be1ef0a6
Robert Krombholz <robert.krombholz@gmx.de>
2014-08-28 22:27:06 +0200

    * addedd build commands for windows & linux 386 and amd64

--- build.sh
+++ build.sh
@@ -3,5 +3,12 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOBIN=${CWD}/bin
 export GOPATH=`godep path`:$GOPATH
 
-go install github.com/rkrombho/getchctl
+#go install github.com/rkrombho/getchctl
+
+cd $CWD/getchctl
+GOOS=linux GOARCH=386 go install
+GOOS=linux GOARCH=amd64 go install
+GOOS=windows GOARCH=386 go install
+GOOS=windows GOARCH=amd64 go install
+
 
